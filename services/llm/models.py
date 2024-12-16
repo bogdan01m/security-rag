3cc2c3086fddde8643eff8dd1d06dec99af394d3
zeinovich <nikita.zinovich@gmail.com>
2024-12-16 20:32:24 +0300

    Changed logging levels

--- services/llm/models.py
+++ services/llm/models.py
@@ -29,7 +29,7 @@ class RAGResponse(BaseModel):
         user_id: int, response: Union[Dict[str, str], Any]
     ) -> RAGResponse:
         if not isinstance(response, dict):
-            logger.info(f"{user_id=} Not dict")
+            logger.warning(f"{user_id=} Not dict")
             return RAGResponse(user_id=user_id)
 
         # Filter only required keys
@@ -38,14 +38,15 @@ class RAGResponse(BaseModel):
             for key, value in response.items()
             if key in RAGResponse.__annotations__
         }
-        logger.info(f"{user_id=} {filtered_response}")
+        logger.debug(f"{user_id=} {filtered_response}")
 
         # Check if all required fields are present and valid
         if all(
             filtered_response.get(key, None) is not None
             for key in list(RAGResponse.__annotations__.keys())[1:]
         ):
-            logger.info(f"{user_id=} Passed test")
+            logger.debug(f"{user_id=} Passed test")
             return RAGResponse(user_id=user_id, **filtered_response)
 
+        logger.warning(f"{user_id=} Didn't pass a test")
         return RAGResponse(user_id=user_id)
